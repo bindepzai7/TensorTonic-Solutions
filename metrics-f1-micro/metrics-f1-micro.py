6a30f344f83bfd0eba03ecbdd5b94771a004182a
Doan Duc Tuan <155902177+bindepzai7@users.noreply.github.com>
2026-03-11 14:57:42 +0700

    Add solution for metrics-f1-micro

---
+++ metrics-f1-micro/metrics-f1-micro.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def f1_micro(y_true, y_pred) -> float:
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    tp = np.sum(y_true == y_pred)
+    total = len(y_true)
+
+    return tp / total
